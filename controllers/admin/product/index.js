a7d4145222edc04e3055d26d5f6687b05e56e32e
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-04-27 00:49:03 +0700

    import delete product

--- controllers/admin/product/index.js
+++ controllers/admin/product/index.js
@@ -4,6 +4,7 @@ const {
   updateStatusProduct,
   getAllProducts,
   getOneProduct,
+  deleteProduct,
 } = require("../../../services/product");
 
 const { StatusCodes } = require("http-status-codes");
@@ -22,4 +23,74 @@ const create = async (req, res, next) => {
   }
 };
 
-module.exports = { create };
+const updateProducts = async (req, res, next) => {
+  try {
+    const result = await updateProduct(req);
+
+    return res.status(StatusCodes.CREATED).json({
+      status: true,
+      message: "Success update product",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateStatus = async (req, res, next) => {
+  try {
+    const result = await updateStatusProduct(req);
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success update status",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const index = async (req, res, next) => {
+  try {
+    const result = await getAllProducts(req);
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success show data",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const show = async (req, res, next) => {
+  try {
+    const result = await getOneProduct(req);
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success show data",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const destroy = async (req, res, next) => {
+  try {
+    const result = await deleteProduct(req);
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success delete data",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { create, updateProducts, updateStatus, index, show, destroy };
