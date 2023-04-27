5059612b5223dcb8269c933b25ca607091a7cca8
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-04-27 23:56:55 +0700

    make endpoint get all user

--- routes/role.js
+++ routes/role.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const role = require("../controllers/owner/role");
 
 router.post("/create", role.create);
+router.get("/get", role.index);
 
 module.exports = router;
