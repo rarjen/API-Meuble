047ee307042320d3b6f50c4021534d8d6299d1cd
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-04-28 00:09:42 +0700

    add middleware authorization

--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const product = require("../controllers/admin/product");
+const authorize = require("../middlewares/authorize");
 
-router.post("/create", product.create);
+router.post("/create", authorize("ADMIN"), product.create);
 router.get("/", product.index);
 router.get("/:product_id", product.show);
-router.put("/edit/:product_id", product.updateProducts);
-router.put("/:product_id", product.updateStatus);
-router.delete("/:product_id", product.destroy);
+router.put("/edit/:product_id", authorize("ADMIN"), product.updateProducts);
+router.put("/:product_id", authorize("ADMIN"), product.updateStatus);
+router.delete("/:product_id", authorize("ADMIN"), product.destroy);
 
 module.exports = router;
