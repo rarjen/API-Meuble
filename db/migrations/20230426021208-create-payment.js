9dc18ffbb0fd5df4af122a19707f372a86186808
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-04-29 01:39:02 +0700

    add fields imagekit_id

--- db/migrations/20230426021208-create-payment.js
+++ db/migrations/20230426021208-create-payment.js
@@ -1,31 +1,34 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Payments', {
+    await queryInterface.createTable("Payments", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       payment: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       img_url: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+      },
+      imagekit_id: {
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Payments');
-  }
+    await queryInterface.dropTable("Payments");
+  },
 };
