20824b5c97837078a389a1d124eb6df8309b4746
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-04-29 01:40:28 +0700

    modify endpoint product

--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const product = require("../controllers/product");
 const authorize = require("../middlewares/authorize");
+const { ROLES } = require("../utils/enum");
 
-router.post("/create", authorize("ADMIN"), product.create);
 router.get("/", product.index);
 router.get("/:product_id", product.show);
-router.put("/edit/:product_id", authorize("ADMIN"), product.updateProducts);
-router.put("/:product_id", authorize("ADMIN"), product.updateStatus);
-router.delete("/:product_id", authorize("ADMIN"), product.destroy);
+router.post("/create", authorize(ROLES.ADMIN), product.create);
+router.put("/edit/:product_id", authorize(ROLES.ADMIN), product.updateProducts);
+router.put("/:product_id", authorize(ROLES.ADMIN), product.updateStatus);
+router.delete("/:product_id", authorize(ROLES.ADMIN), product.destroy);
 
 module.exports = router;
