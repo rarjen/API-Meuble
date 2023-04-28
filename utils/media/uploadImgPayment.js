d266ab33ee2d260b57cf92d5f35c208f2413b951
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-04-29 01:43:05 +0700

    create upload image method

---
+++ utils/media/uploadImgPayment.js
@@ -0,0 +1,23 @@
+const imagekit = require("./imagekit");
+
+const uploadImgPayment = async (file) => {
+  try {
+    const uploadFile = await imagekit.upload({
+      file,
+      fileName: `picture`,
+    });
+
+    const data = {
+      uploadFile: uploadFile,
+      title: uploadFile.name,
+      url: uploadFile.url,
+    };
+
+    console.log(data.uploadFile.fileId);
+    return data;
+  } catch (error) {
+    throw new error();
+  }
+};
+
+module.exports = uploadImgPayment;
