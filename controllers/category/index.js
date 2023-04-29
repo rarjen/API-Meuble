7b8ea745fb9f4e22edfed6952e77202af1172c40
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-04-29 12:11:37 +0700

    create controller for category

---
+++ controllers/category/index.js
@@ -0,0 +1,80 @@
+const {
+  createCategory,
+  getAllCategories,
+  getOneCategory,
+  deleteCategory,
+  editCategory,
+} = require("../../services/category");
+const { StatusCodes } = require("http-status-codes");
+
+const create = async (req, res, next) => {
+  try {
+    const result = await createCategory(req);
+
+    return res.status(StatusCodes.CREATED).json({
+      status: true,
+      message: "Success create category",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const index = async (req, res, next) => {
+  try {
+    const result = await getAllCategories();
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success get all categories",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const show = async (req, res, next) => {
+  try {
+    const result = await getOneCategory(req);
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success get category",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const update = async (req, res, next) => {
+  try {
+    const result = await editCategory(req);
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success edit category",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const destroy = async (req, res, next) => {
+  try {
+    const result = await deleteCategory(req);
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success delete category",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { create, index, show, update, destroy };
