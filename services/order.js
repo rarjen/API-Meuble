1e6aabc72ad003a2265b12f26c25b27cea212a01
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-12 00:10:23 +0700

    create order services

---
+++ services/order.js
@@ -0,0 +1,65 @@
+const { Order, Product, Order_detail, Transaction } = require("../models");
+const { BadRequestError, NotFoundError } = require("../errors");
+
+const createOrder = async (req) => {
+  const user = req.user;
+  const { product_id } = req.query;
+  const { qty, note } = req.body;
+
+  const checkProduct = await Product({ where: { id: product_id } });
+
+  if (!checkProduct) {
+    throw new NotFoundError(`Tidak ada product dengan id: ${product_id}`);
+  }
+
+  const result = await Order.create({
+    user_id: user.id,
+    product_id: product_id,
+    qty,
+    note,
+  });
+
+  await Order_detail.create({
+    order_id: result.id,
+    total: checkProduct.harga * qty,
+  });
+
+  return result;
+};
+
+const deleteOrder = async (req) => {
+  const { order_id } = req.params;
+
+  const checkOrder = await Order.findOne({ where: { id: order_id } });
+  const checkOrderDetail = await Order_detail.findOne({ where: { order_id } });
+
+  if (!checkOrder && !checkOrderDetail) {
+    throw new NotFoundError(`Tidak ada order dengan id: ${order_id}`);
+  }
+
+  const result = await Order.destroy({ where: { id: order_id } });
+  await Order_detail.destroy({ where: { order_id } });
+
+  return result;
+};
+
+const readOrderByAdmin = async (req) => {
+  const { start_date, end_date } = req.query;
+
+  // const result = await Order.
+};
+
+const readOrderByUser = async (req) => {
+  const user = req.user;
+
+  const result = await Order.findAll({ where: { user_id: user.id } });
+
+  return result;
+};
+
+module.exports = {
+  createOrder,
+  readOrderByAdmin,
+  readOrderByUser,
+  deleteOrder,
+};
