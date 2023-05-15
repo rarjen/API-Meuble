e1eb55c7cb975e4f76a427fa0c70cca420369fa8
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-15 11:51:51 +0700

    add association

--- models/order_detail.js
+++ models/order_detail.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Order_detail extends Model {
     /**
@@ -10,15 +8,21 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Order_detail.belongsTo(models.Order, {
+        foreignKey: "order_id",
+        as: "order",
+      });
     }
   }
-  Order_detail.init({
-    order_id: DataTypes.INTEGER,
-    total: DataTypes.DOUBLE
-  }, {
-    sequelize,
-    modelName: 'Order_detail',
-  });
+  Order_detail.init(
+    {
+      order_id: DataTypes.INTEGER,
+      total: DataTypes.DOUBLE,
+    },
+    {
+      sequelize,
+      modelName: "Order_detail",
+    }
+  );
   return Order_detail;
 };
