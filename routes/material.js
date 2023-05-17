ece3d0e4498fff3ab407ed912a98fff23cc39747
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-17 14:42:33 +0700

    make routes for material

---
+++ routes/material.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const material = require("../controllers/material");
+const authorize = require("../middlewares/authorize");
+const { ROLES } = require("../utils/enum");
+
+router.post("/create", authorize(ROLES.ADMIN), material.create);
+router.put("/:material_id", authorize(ROLES.ADMIN), material.destroy);
+router.put("/edit/:material_id", authorize(ROLES.ADMIN), material.update);
+router.get("/all-materials", material.index);
+router.get(
+  "/material-categories/:category_id",
+  material.readMaterialByCategory
+);
+router.get("/:material_id", material.show);
+
+module.exports = router;
