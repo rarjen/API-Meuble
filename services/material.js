9bcb89659ca93e5cc3502f5d577cb45b0f763a05
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-17 14:43:37 +0700

    make material service

--- services/material.js
+++ services/material.js
@@ -1,4 +1,4 @@
-const { Material } = require("../models");
+const { Material, Category } = require("../models");
 const { BadRequestError, NotFoundError } = require("../errors");
 const { VARIANT } = require("../utils/enum");
 const { Op } = require("sequelize");
@@ -52,7 +52,10 @@ const editMaterial = async (req) => {
 };
 
 const readAllMaterial = async () => {
-  const result = await Material.findAll({ where: { status: VARIANT.ACTIVE } });
+  const result = await Material.findAll({
+    where: { status: VARIANT.ACTIVE },
+    include: [{ model: Category, as: "category" }],
+  });
 
   return result;
 };
@@ -62,6 +65,7 @@ const readOneMaterial = async (req) => {
 
   const result = await Material.findOne({
     where: { id: material_id, status: VARIANT.ACTIVE },
+    include: [{ model: Category, as: "category" }],
   });
 
   if (!result) {
@@ -87,10 +91,28 @@ const destroyMaterial = async (req) => {
   return result;
 };
 
+const readByCategory = async (req) => {
+  const { category_id } = req.params;
+
+  const result = await Material.findAll({
+    where: { category_id, status: VARIANT.ACTIVE },
+    include: [{ model: Category, as: "category" }],
+  });
+
+  if (result <= 0) {
+    throw new NotFoundError(
+      `Tidak ada size dengan kategori id: ${category_id}`
+    );
+  }
+
+  return result;
+};
+
 module.exports = {
   createMaterial,
   editMaterial,
   readAllMaterial,
   readOneMaterial,
   destroyMaterial,
+  readByCategory,
 };
