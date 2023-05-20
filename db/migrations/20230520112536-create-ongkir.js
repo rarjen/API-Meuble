a26201bb93c48fafd0b0e29a75e815a60f4ff8bd
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-20 19:30:38 +0700

    make table ongkir

---
+++ db/migrations/20230520112536-create-ongkir.js
@@ -0,0 +1,30 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Ongkirs", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      transaction_id: {
+        type: Sequelize.INTEGER,
+      },
+      ongkir: {
+        type: Sequelize.DOUBLE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Ongkirs");
+  },
+};
