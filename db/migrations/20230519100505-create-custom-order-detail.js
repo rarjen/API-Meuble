4015cfffbd53d25ca1ba24e45ce81fa283338049
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-20 11:40:54 +0700

    create customorder detail table

---
+++ db/migrations/20230519100505-create-custom-order-detail.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Custom_order_details', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      custom_order_id: {
+        type: Sequelize.INTEGER
+      },
+      price: {
+        type: Sequelize.DOUBLE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Custom_order_details');
+  }
+};
