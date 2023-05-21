04520b557c591648c14f1beb19e534ea74f8bfb3
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-21 14:47:10 +0700

    create models for city

---
+++ models/city.js
@@ -0,0 +1,28 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class City extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  City.init(
+    {
+      province_id: DataTypes.INTEGER,
+      province: DataTypes.STRING,
+      type: DataTypes.STRING,
+      city_name: DataTypes.STRING,
+      postal_code: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "City",
+    }
+  );
+  return City;
+};
