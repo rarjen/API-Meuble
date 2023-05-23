daf941f59a605770771819d47fd9de5285a0021b
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-23 09:08:03 +0700

    modify routes for user

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,8 @@ const user = require("../controllers/user");
 const authorize = require("../middlewares/authorize");
 const { ROLES } = require("../utils/enum");
 
-router.get("/", authorize(ROLES.ADMIN), user.index);
+router.get("/all-users", authorize(ROLES.ADMIN), user.index);
 router.put("/update-bio", authorize(), user.createBio);
 router.put("/reset-password", authorize(), user.resetPasswordUser);
-router.post("/create-address", authorize(), user.createAddressUser);
-router.put("/edit-address/:address_id", authorize(), user.updateAddressUser);
-router.get("/address", authorize(), user.getAddress);
 
 module.exports = router;
