0aa1db034fb4ef3ef802f7927acd4b6b4b70cd1b
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-25 14:40:26 +0700

    make routes for cancel and read by user

--- routes/transaction.js
+++ routes/transaction.js
@@ -3,7 +3,9 @@ const transaction = require("../controllers/transaction");
 const authorize = require("../middlewares/authorize");
 const { ROLES } = require("../utils/enum");
 
-router.post("/create", authorize(ROLES.BUYER), transaction.create);
 router.get("/all-transactions", authorize(ROLES.ADMIN), transaction.indexAdmin);
+router.put("/cancel/:transaction_id", authorize(), transaction.cancel);
+router.post("/create", authorize(ROLES.BUYER), transaction.create);
+router.get("/", authorize(ROLES.BUYER), transaction.indexAdmin);
 
 module.exports = router;
