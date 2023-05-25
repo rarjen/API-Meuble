a046878737fc78ae56040ffec657b50ce267b443
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-25 14:03:48 +0700

    modify models

--- models/transaction.js
+++ models/transaction.js
@@ -2,24 +2,48 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Transaction extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      Transaction.belongsTo(models.Order, {
-        foreignKey: "order_id",
-        as: "order",
+      Transaction.belongsTo(models.User, {
+        foreignKey: "user_id",
+        as: "user",
+      });
+
+      Transaction.belongsTo(models.Product, {
+        foreignKey: "product_id",
+        as: "product",
+      });
+
+      Transaction.belongsTo(models.Courrier, {
+        foreignKey: "courrier_id",
+        as: "courrier",
+      });
+
+      Transaction.belongsTo(models.Payment, {
+        foreignKey: "payment_id",
+        as: "payment",
+      });
+
+      Transaction.hasOne(models.Image_transaction, {
+        foreignKey: "transaction_id",
+        as: "img_transaction",
       });
     }
   }
   Transaction.init(
     {
       user_id: DataTypes.INTEGER,
-      order_id: DataTypes.INTEGER,
-      courrier_id: DataTypes.INTEGER,
+      invoice_number: DataTypes.STRING,
+      product_id: DataTypes.INTEGER,
+      qty: DataTypes.INTEGER,
+      note: DataTypes.TEXT,
       payment_id: DataTypes.INTEGER,
+      courrier_id: DataTypes.INTEGER,
+      service: DataTypes.STRING,
+      total_weight: DataTypes.DOUBLE,
+      total: DataTypes.DOUBLE,
+      ongkir: DataTypes.DOUBLE,
+      grandTotal: DataTypes.DOUBLE,
+      nomerResi: DataTypes.STRING,
       status: DataTypes.STRING,
     },
     {
