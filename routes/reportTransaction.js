f19a79aa6f00c432b7461d2a8ff24565cea17d44
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-26 21:14:15 +0700

    modify download report routes

--- routes/reportTransaction.js
+++ routes/reportTransaction.js
@@ -3,6 +3,6 @@ const reportTransaction = require("../controllers/reportTransaction");
 const authorize = require("../middlewares/authorize");
 const { ROLES } = require("../utils/enum");
 
-router.get("/", reportTransaction.create);
+router.get("/", authorize(ROLES.ADMIN), reportTransaction.create);
 
 module.exports = router;
