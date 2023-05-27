407071aa9ea3df8cfb9daabb7b2f8c842a968096
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-27 13:33:32 +0700

    modify routes transaction

--- routes/transaction.js
+++ routes/transaction.js
@@ -5,6 +5,12 @@ const { ROLES } = require("../utils/enum");
 
 router.get("/all-transactions", authorize(ROLES.ADMIN), transaction.indexAdmin);
 router.put("/cancel/:transaction_id", authorize(), transaction.cancel);
+router.put("/resi/:transaction_id", authorize(), transaction.resi);
+router.put(
+  "/update/:transaction_id",
+  authorize(ROLES.ADMIN),
+  transaction.update
+);
 router.post("/create", authorize(ROLES.BUYER), transaction.create);
 router.get("/", authorize(ROLES.BUYER), transaction.indexAdmin);
 
