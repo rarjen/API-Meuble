83bffeca519a7aab26fe5d15f0c3d74c6332f4db
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-28 00:33:15 +0700

    create new table for rekening

---
+++ db/migrations/20230527173220-create-rekening.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Rekenings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      payment_id: {
+        type: Sequelize.INTEGER
+      },
+      rekening: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Rekenings');
+  }
+};
