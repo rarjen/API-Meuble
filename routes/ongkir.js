1a2b517d93ea2ab5b1d97769e7a7861dcb21c6f1
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-28 19:45:47 +0700

    modify ongkir routes

--- routes/ongkir.js
+++ routes/ongkir.js
@@ -3,5 +3,6 @@ const ongkir = require("../controllers/checkCost");
 const authorize = require("../middlewares/authorize");
 
 router.post("/check", authorize(), ongkir.ongkir);
+router.post("/check-cod", authorize(), ongkir.cod);
 
 module.exports = router;
