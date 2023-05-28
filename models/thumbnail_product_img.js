d05bb4555f4dfd993507794b3f284ac1036a4b7d
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-28 22:36:49 +0700

    make model for thumbnails

---
+++ models/thumbnail_product_img.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Thumbnail_product_img extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Thumbnail_product_img.init({
+    product_id: DataTypes.INTEGER,
+    img_url: DataTypes.STRING,
+    imagekit_id: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Thumbnail_product_img',
+  });
+  return Thumbnail_product_img;
+};
