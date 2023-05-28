649aafda864a9410f6abca3baec4aa3321cfb2f3
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-28 19:45:32 +0700

    modify routes coordinate

--- routes/coordinate.js
+++ routes/coordinate.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const coordinate = require("../controllers/coordinate");
 const authorize = require("../middlewares/authorize");
-const { ROLES } = require("../utils/enum");
 
 router.post("/create", authorize(), coordinate.create);
 
