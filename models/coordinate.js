03607f671afc926f3f81adff8b371b2f8c5ade88
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-28 12:15:12 +0700

    create models for coordinate

---
+++ models/coordinate.js
@@ -0,0 +1,29 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Coordinate extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Coordinate.belongsTo(models.Address, {
+        foreignKey: "address_id",
+        as: "address",
+      });
+    }
+  }
+  Coordinate.init(
+    {
+      address_id: DataTypes.INTEGER,
+      lat: DataTypes.DECIMAL,
+      lng: DataTypes.DECIMAL,
+    },
+    {
+      sequelize,
+      modelName: "Coordinate",
+    }
+  );
+  return Coordinate;
+};
