16f3709fc0bc241380af78a588edc2b83559f64d
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-28 11:38:57 +0700

    modify service for address

--- services/address.js
+++ services/address.js
@@ -17,7 +17,12 @@ const createAddress = async (req) => {
 
   const checkAddress = await Address.findOne({ where: { user_id: user.id } });
   if (checkAddress) {
-    throw new BadRequestError(`Anda hanya boleh memiliki 1 alamat`);
+    const result = await Address.update(
+      { province_id, city_id, address },
+      { where: { user_id: user.id } }
+    );
+
+    return result;
   }
 
   const result = await Address.create({
@@ -28,33 +33,6 @@ const createAddress = async (req) => {
   });
 
   await User.update({ address_id: result.id }, { where: { id: user.id } });
-
-  return result;
-};
-
-const editAddress = async (req) => {
-  const { address_id } = req.params;
-  const { province_id, city_id, address } = req.body;
-
-  const checkAddress = await Address.findOne({ where: { id: address_id } });
-  if (!checkAddress) {
-    throw new NotFoundError(`Tidak ada address dengan id: ${address_id}`);
-  }
-
-  const checkProvince = await Province.findOne({ where: { id: province_id } });
-  if (!checkProvince) {
-    throw new NotFoundError(`Tidak ada province dengan id: ${province_id}`);
-  }
-
-  const checkCity = await City.findOne({ where: { id: city_id } });
-  if (!checkCity) {
-    throw new NotFoundError(`Tidak ada city dengan id: ${city_id}`);
-  }
-
-  const result = await Address.update(
-    { province_id, city_id, address },
-    { where: { id: address_id } }
-  );
 
   return result;
 };
@@ -73,4 +51,4 @@ const getAddressUser = async (req) => {
   return result;
 };
 
-module.exports = { createAddress, editAddress, getAddressUser };
+module.exports = { createAddress, getAddressUser };
