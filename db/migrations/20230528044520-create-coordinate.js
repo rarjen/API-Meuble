df6adb2fd9c4f985f6c81750a4d7da68f7d35c69
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-28 12:14:32 +0700

    create migration for coordinate

---
+++ db/migrations/20230528044520-create-coordinate.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Coordinates', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      address_id: {
+        type: Sequelize.INTEGER
+      },
+      lat: {
+        type: Sequelize.DECIMAL
+      },
+      lng: {
+        type: Sequelize.DECIMAL
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Coordinates');
+  }
+};
