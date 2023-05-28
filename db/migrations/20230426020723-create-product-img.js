51fc338fc6ee66515bb06cde33c1e33717199121
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-28 22:34:16 +0700

    modify migration product img

--- db/migrations/20230426020723-create-product-img.js
+++ db/migrations/20230426020723-create-product-img.js
@@ -1,31 +1,34 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Product_imgs', {
+    await queryInterface.createTable("Product_imgs", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       product_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       img_url: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+      },
+      imagekit_id: {
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Product_imgs');
-  }
+    await queryInterface.dropTable("Product_imgs");
+  },
 };
