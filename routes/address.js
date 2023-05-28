3b7fdccf67d7d50094ae486ca743c76f17165d1c
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-28 11:39:09 +0700

    modify routes for address

--- routes/address.js
+++ routes/address.js
@@ -3,7 +3,6 @@ const address = require("../controllers/address");
 const authorize = require("../middlewares/authorize");
 
 router.post("/create-address", authorize(), address.createAddressUser);
-router.put("/edit-address/:address_id", authorize(), address.updateAddressUser);
 router.get("/", authorize(), address.getAddress);
 
 module.exports = router;
