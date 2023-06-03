7ed67925222b3919e26bc91f7eda2e01d405f78f
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-03 15:23:50 +0700

    modify service upload Transaction Custom Order

--- services/uploadTransactionPicture.js
+++ services/uploadTransactionPicture.js
@@ -6,21 +6,32 @@ const uploadImageTransaction = async (req) => {
   const { transaction_id } = req.body;
   const file = req.file.buffer.toString("base64");
 
-  const integerTransactionId = parseInt(transaction_id);
-
   const checkTransaction = await Transaction.findOne({
-    where: { id: integerTransactionId },
+    where: { id: transaction_id },
   });
   if (!checkTransaction) {
-    throw new NotFoundError(
-      `Tidak ada transaksi dengan id: ${integerTransactionId}`
-    );
+    throw new NotFoundError(`Tidak ada transaksi dengan id: ${transaction_id}`);
+  }
+
+  const checkPicture = await Image_transaction.findOne({
+    where: { transaction_id },
+  });
+
+  if (checkPicture) {
+    const dataUpload = await uploadImgPayment(file);
+
+    const result = await Image_transaction.update({
+      transaction_id: transaction_id,
+      img_url: dataUpload.url,
+    });
+
+    return result;
   }
 
   const checkPayment = await Payment.findOne({
     where: { id: checkTransaction.payment },
   });
-  if (checkPayment.payment === "COD") {
+  if (checkPayment.payment !== "COD") {
     throw new BadRequestError(
       `Upload bukti transaksi hanya untuk transfer bank!`
     );
@@ -29,7 +40,7 @@ const uploadImageTransaction = async (req) => {
   const dataUpload = await uploadImgPayment(file);
 
   const result = await Image_transaction.create({
-    transaction_id: integerTransactionId,
+    transaction_id: transaction_id,
     img_url: dataUpload.url,
   });
 
