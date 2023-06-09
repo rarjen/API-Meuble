8710a12f731bcc8783fc6afc2f6f947abcdef403
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-09 23:32:10 +0700

    modify service user

--- services/user.js
+++ services/user.js
@@ -1,13 +1,18 @@
-const { Role, User, Address, City, Province } = require("../models");
+const {
+  Role,
+  User,
+  Address,
+  City,
+  Province,
+  Coordinate,
+} = require("../models");
 const { BadRequestError } = require("../errors");
-const bcrypt = require("bcrypt");
-const Validator = require("fastest-validator");
-const v = new Validator();
-const { Op } = require("sequelize");
+
+const { Op, fn } = require("sequelize");
 
 const updateBio = async (req) => {
   const user = req.user;
-  const { first_name, last_name, email, mobile } = req.body;
+  const { first_name, last_name, mobile } = req.body;
 
   const checkMobile = await User.findOne({
     where: {
@@ -17,22 +22,11 @@ const updateBio = async (req) => {
   });
 
   if (checkMobile) {
-    throw new BadRequestError("Email / Nomer Telepon sudah terdaftar!");
-  }
-
-  const checkEmail = await User.findOne({
-    where: {
-      email: email,
-      id: { [Op.ne]: user.id },
-    },
-  });
-
-  if (checkEmail) {
-    throw new BadRequestError("Email / Nomer Telepon sudah terdaftar!");
+    throw new BadRequestError("Nomer Telepon sudah terdaftar!");
   }
 
   const result = await User.update(
-    { first_name, last_name, email, mobile },
+    { first_name, last_name, mobile },
     { where: { id: user.id } }
   );
 
@@ -46,10 +40,10 @@ const getAllUser = async (req) => {
   if (search) {
     where = {
       [Op.or]: [
-        { email: { [Op.like]: "%" + search + "%" } },
-        { first_name: { [Op.like]: "%" + search + "%" } },
-        { last_name: { [Op.like]: "%" + search + "%" } },
-        { mobile: { [Op.like]: "%" + search + "%" } },
+        { email: { [Op.like]: fn("LOWER", `%${search}%`) } },
+        { first_name: { [Op.like]: fn("LOWER", `%${search}%`) } },
+        { last_name: { [Op.like]: fn("LOWER", `%${search}%`) } },
+        { mobile: { [Op.like]: fn("LOWER", `%${search}%`) } },
       ],
     };
   }
@@ -81,6 +75,10 @@ const getAllUser = async (req) => {
             model: City,
             as: "city",
           },
+          {
+            model: Coordinate,
+            as: "coordinate",
+          },
         ],
       },
     ],
@@ -95,39 +93,7 @@ const getAllUser = async (req) => {
   };
 };
 
-const resetPassword = async (req) => {
-  const user = req.user;
-  const { new_password, confirm_password } = req.body;
-
-  const schema = {
-    new_password: { type: "string", min: 6 },
-    confirm_password: { type: "string", min: 6 },
-  };
-  const check = await v.compile(schema);
-  const validate = check({
-    new_password: `${new_password}`,
-    confirm_password: `${confirm_password}`,
-  });
-  if (validate.length > 0) {
-    throw new BadRequestError("Password at least 6 characters!");
-  }
-
-  if (new_password !== confirm_password) {
-    throw new BadRequestError("Password harus sama!");
-  }
-
-  const encryptedPassword = await bcrypt.hash(new_password, 10);
-
-  const result = await User.update(
-    { password: encryptedPassword },
-    { where: { id: user.id } }
-  );
-
-  return result;
-};
-
 module.exports = {
   getAllUser,
   updateBio,
-  resetPassword,
 };
