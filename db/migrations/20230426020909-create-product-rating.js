154569fb4fabe839ad9e5606b5df488e3e042a6d
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-11 00:12:22 +0700

    modify migrations

--- db/migrations/20230426020909-create-product-rating.js
+++ db/migrations/20230426020909-create-product-rating.js
@@ -1,34 +1,37 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Product_ratings', {
+    await queryInterface.createTable("Product_ratings", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       user_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       product_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+      },
+      transaction_id: {
+        type: Sequelize.INTEGER,
       },
       rating: {
-        type: Sequelize.FLOAT
+        type: Sequelize.FLOAT,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Product_ratings');
-  }
+    await queryInterface.dropTable("Product_ratings");
+  },
 };
