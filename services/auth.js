924e4800375d61bf102db5dca862f04476f9962f
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-12 14:08:43 +0700

    modify auth service

--- services/auth.js
+++ services/auth.js
@@ -93,6 +93,8 @@ const login = async (req) => {
 
   const payload = {
     id: user.id,
+    first_name: user.first_name,
+    last_name: user.last_name,
     email: user.email,
     role: user.role.role,
   };
