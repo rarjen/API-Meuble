8d9933a2da96b3a0fc841b31e88cb0ed443b5be8
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-16 17:40:36 +0700

    modify routes for category

--- routes/category.js
+++ routes/category.js
@@ -5,6 +5,7 @@ const { ROLES } = require("../utils/enum");
 
 router.post("/create", authorize(ROLES.ADMIN), category.create);
 router.put("/edit/:category_id", authorize(ROLES.ADMIN), category.update);
+router.put("/delete/:category_id", authorize(ROLES.ADMIN), category.destroy);
 router.get("/", category.index);
 router.get("/:category_id", category.show);
 
