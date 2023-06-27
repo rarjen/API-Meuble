1431fc4f4efad4e11c0339692d5c1b5ed6a0e334
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-27 10:20:01 +0700

    modify sale route

--- routes/sale.js
+++ routes/sale.js
@@ -4,6 +4,5 @@ const authorize = require("../middlewares/authorize");
 const { ROLES } = require("../utils/enum");
 
 router.get("/", authorize(ROLES.ADMIN), sale.total);
-router.get("/all-sales", authorize(ROLES.ADMIN), sale.index);
 
 module.exports = router;
