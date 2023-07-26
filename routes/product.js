604d5f2a5164376e068fbeafa4318ecb35d840ab
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-07-26 16:27:37 +0700

    modify product routes

--- routes/product.js
+++ routes/product.js
@@ -4,8 +4,9 @@ const authorize = require("../middlewares/authorize");
 const { ROLES } = require("../utils/enum");
 
 router.get("/show/:product_id", product.show);
-router.get('/bestseller', product.getBestSeller);
+router.get("/bestseller", product.getBestSeller);
 router.get("/", product.getByUser);
+router.get("/show/category/:id_category", product.getProductByCategory);
 router.get("/index", authorize(ROLES.ADMIN), product.getByAdmin);
 router.post("/create", authorize(ROLES.ADMIN), product.create);
 router.put("/edit/:product_id", authorize(ROLES.ADMIN), product.updateProducts);
