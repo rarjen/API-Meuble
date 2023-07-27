fc61553608fc3fc284f332514440afdbf459c5e7
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-07-27 12:33:49 +0700

    modify routes

--- routes/customTransaction.js
+++ routes/customTransaction.js
@@ -56,10 +56,4 @@ router.put(
   customTransaction.updateDoneAdmin
 );
 
-router.put(
-  "/update-status-payment/paid/:transaction_custom_order_id",
-  authorize(ROLES.ADMIN),
-  customTransaction.updatePayment
-);
-
 module.exports = router;
